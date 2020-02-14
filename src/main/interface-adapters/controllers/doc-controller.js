7c30ff7669b7ef607fc47b5a4d01167ecefc8019
Denis De Nadai Carraro <denadaicarraro@gmail.com>
2020-02-14 17:55:01 -0300

    lint file

--- src/main/interface-adapters/controllers/doc-controller.js
+++ src/main/interface-adapters/controllers/doc-controller.js
@@ -1,20 +1,21 @@
 const raml2html = require('raml2html')
 
 module.exports = (logger, server) => {
-    const get = (req, res) => {
-        const configWithDefaultTheme = raml2html.getConfigForTheme()
-        raml2html.render('doc.raml', configWithDefaultTheme).then(function (result) {
-            res.send(result)
-        }, function (error) {
-            res.send(`${error}`)
-        })
-    }
+  const get = (req, res) => {
+    logger.log('info', `docController: Received GET on /`)
+    const configWithDefaultTheme = raml2html.getConfigForTheme()
+    raml2html.render('doc.raml', configWithDefaultTheme).then(function (result) {
+      res.send(result)
+    }, function (error) {
+      res.send(`${error}`)
+    })
+  }
 
-    const map = () => {
-        server.get('/', get)
-    }
+  const map = () => {
+    server.get('/', get)
+  }
 
-    return {
-        map: map
-    }
+  return {
+    map: map
+  }
 }
